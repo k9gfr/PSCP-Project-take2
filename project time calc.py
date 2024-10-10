1fabaaa142e71da24da69c87cb19a6c47b2c4a88
k9g-fr <kongssroekdee@gmail.com>
2024-10-10 22:33:07 +0700

    Update project time calc.py

--- project time calc.py
+++ project time calc.py
@@ -4,8 +4,9 @@ UTC_MINUS_10 = ["honolulu","french polynesia", "cook islands", "aleutian islands
 UTC_MINUS_9_30 = ["marquesas islands"]
 UTC_MINUS_9 = ["anchorage","gambier islands", "alaska"]
 UTC_MINUS_8= ["british columbia", "clipperton island", "baja california", "pitcairn islands", "california", "idaho", "nevada", "oregon", "washington"]
-UTC_MINUS_7 = ["denver", "calgary", "ciudad juarez","canada", "alberta", 
+UTC_MINUS_7 = ["denver", "calgary", "ciudad juarez","canada", "alberta",
 "nortern rockies regional municipality", "peace river regional district"
 , "cranbrook", "golden", "invermere", "northwest territories", "kitikmeot region",
- "lloydminster", "yukon", "baja california sur", "chihuahua", "nayarit", "sinaloa", "sonora"]
+"lloydminster", "yukon", "baja california sur", "chihuahua", "nayarit", "sinaloa",
+"sonora", "kansas", "montana", "nebraska", "west wendover", "new mexico", "north dakota", "malheur country", "malheur", "south dakota", "texas", "utah", "wyoming"]
 UTC_MINUS_6 = []
