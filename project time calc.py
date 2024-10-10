2bbc9251f5e66e6fb2bf43cdbbfa5d961f428c3a
k9g-fr <kongssroekdee@gmail.com>
2024-10-10 22:28:06 +0700

    Update project time calc.py

--- project time calc.py
+++ project time calc.py
@@ -1,2 +1,11 @@
 UTC_MINUS_12 = ["baker island", "howland island"]
-UTC_MINUS_11 = ["niue","american samoa"]
+UTC_MINUS_11 = ["niue","american samoa","jarvis island", "kingman reef", "midway atoll", "palmyra atoll"]
+UTC_MINUS_10 = ["honolulu","french polynesia", "cook islands", "aleutian islands", "hawaii", "johnston atoll"]
+UTC_MINUS_9_30 = ["marquesas islands"]
+UTC_MINUS_9 = ["anchorage","gambier islands", "alaska"]
+UTC_MINUS_8= ["british columbia", "clipperton island", "baja california", "pitcairn islands", "california", "idaho", "nevada", "oregon", "washington"]
+UTC_MINUS_7 = ["denver", "calgary", "ciudad juarez","canada", "alberta", 
+"nortern rockies regional municipality", "peace river regional district"
+, "cranbrook", "golden", "invermere", "northwest territories", "kitikmeot region",
+ "lloydminster", "yukon", "baja california sur", "chihuahua", "nayarit", "sinaloa", "sonora"]
+UTC_MINUS_6 = []
