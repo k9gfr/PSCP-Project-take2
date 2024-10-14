e96c18eb0e41d83a5cf382ebd1621167dc695ac3
Serjang <67070239@kmitl.ac.th>
2024-10-14 14:11:36 +0700

    Update project time calc.py

--- project time calc.py
+++ project time calc.py
@@ -10,16 +10,16 @@ UTC_MINUS_7 = ["denver", "calgary", "ciudad juarez","canada", "alberta",
 "lloydminster", "yukon", "baja california sur", "chihuahua", "nayarit", "sinaloa",
 "sonora", "kansas", "montana", "nebraska", "west wendover", "new mexico", "north dakota", "malheur country", "malheur", "south dakota", "texas", "utah", "wyoming"]
 UTC_MINUS_6 = ["belize", "manitoba", "nunavut", "ontario", "saskatchewan",
-"chile", "easter island", "costa rica", "ecuador", "galápagos islands",
+"chile", "easter island", "costa rica", "ecuador", "galapagos islands",
 "el salvador", "guatemala", "honduras", "mexico", "nicaragua", "alabama",
 "arkansas", "florida", "illinois", "indiana", "iowa", "kansas", "kentucky",
 "louisiana", "michigan", "minnesota", "mississippi", "missouri", "nebraska", "north dakota", "oklahoma", "south dakota", "tennessee", "texas", "wisconsin"]
-UTC_MINUS_5 = ["New York, NY", "Washington, D.C.", "Atlanta, GA", "Miami, FL",
-"Philadelphia, PA", "Boston, MA", "Charlotte, NC", "Cleveland, OH",
-"Indianapolis, IN", "Nashville, TN", "Detroit, MI",
-"Toronto, ON", "Ottawa, ON", "Montreal, QC", "Quebec City, QC",
-"Hamilton, ON", "Kitchener, ON", "Havana, Cuba", "Kingston, Jamaica", "Port-au-Prince, Haiti", "Cayman Islands", "Turks and Caicos Islands",
- "Bogotá, Colombia", "Lima, Peru", "Quito, Ecuador","Nassau, Bahamas", "Manaus, Brazil", "Acre, Brazil","Santa Cruz, Bolivia"]
+UTC_MINUS_5 = ["new york, ny", "washington, d.c.", "atlanta, ga", "miami, fl",
+"philadelphia, pa", "boston, ma", "charlotte, nc", "cleveland, oh",
+"indianapolis, in", "nashville, tn", "detroit, mi",
+"toronto, on", "ottawa, on", "montreal, qc", "quebec city, qc",
+"hamilton, on", "kitchener, on", "havana, cuba", "kingston, jamaica", "port-au-prince, haiti", "cayman islands", "turks and caicos islands",
+"bogota, colombia", "lima, peru", "quito, ecuador", "nassau, bahamas", "manaus, brazil", "acre, brazil", "santa cruz, bolivia"]
 UTC_MINUS_4 = ["santiago", "santo domingo", "manaus", "caracas", "la paz", 
 "halifax", "Antigua and Barbuda", "Barbados", "Bolivia", "Mato Grosso, Brazil", "Mato Grosso do Sul, Brazil", "Rondônia, Brazil", "Roraima, Brazil"
 "New Brunswick, Canada", "Newfoundland and Labrador, Canada", ""]
